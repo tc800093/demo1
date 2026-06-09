cc5715094cdc742b21ab943e24841ad8cdeb7499
tc800093 <123717192+tc800093@users.noreply.github.com>
2026-06-09 22:19:11 +0530

    Delete lib directory

--- lib/domain/repositories/data_repository.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import '../entities/entities.dart';
-
-abstract class DataRepository {
-  Future<Either<String, List<PowerMetric>>> getMetrics();
-  Future<Either<String, List<OutageLog>>> getOutages();
-  Future<Either<String, List<FuelLog>>> getFuelLogs();
-}
